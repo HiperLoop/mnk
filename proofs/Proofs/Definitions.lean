63c3826bd8f927f6db18ead9e31d227675a5332e
Adam Zich <adam.zich3@gmail.com>
2026-03-23 18:50:40 +0100

    tidiet up definitions file

--- proofs/Proofs/Definitions.lean
+++ proofs/Proofs/Definitions.lean
@@ -1,17 +1,10 @@
 import Mathlib.Data.Set.Basic
 
+-- 1D
 -- Defines what a line that corresponds to a winning length k is in 1D.
 def Is_winning_line_1D (k : ℕ) (L : Set Int) : Prop :=
   ∃ (Start : Int) (dir : Int), (dir = 1 ∨ dir = -1) ∧
   L = { c | ∃ (i : ℕ), i < k ∧ c = Start + i * dir }
-
-def Coord := Int × Int
-
--- Defines what a line that corresponds to a winning length k is in 2D.
-def Is_winning_line_2D (k : ℕ) (L : Set Coord) : Prop :=
-  ∃ (start : Coord) (dir : Int × Int),
-    (dir.1 ≠ 0 ∨ dir.2 ≠ 0) ∧
-    L = { p | ∃ (i : ℤ), 0 ≤ i ∧ i < k ∧ p = (start.1 + i * dir.1, start.2 + i * dir.2) }
 
 -- Defines what a pairing strategy is in 1D.
 structure Pairing_strategy_1D (k : ℕ) where
@@ -19,6 +12,15 @@ structure Pairing_strategy_1D (k : ℕ) where
   is_pair : ∀ s ∈ pairs, ∃ a b, a ≠ b ∧ s = {a, b}
   disjoint_logic : ∀ s1 s2, s1 ∈ pairs → s2 ∈ pairs → s1 ≠ s2 → (∀ x, x ∈ s1 → x ∈ s2 → False)
   blocks_all : ∀ L, Is_winning_line_1D k L → ∃ s ∈ pairs, s ⊆ L
+
+-- 2D
+def Coord := Int × Int
+
+-- Defines what a line that corresponds to a winning length k is in 2D.
+def Is_winning_line_2D (k : ℕ) (L : Set Coord) : Prop :=
+  ∃ (start : Coord) (dir : Int × Int),
+    (dir.1 ≠ 0 ∨ dir.2 ≠ 0) ∧
+    L = { p | ∃ (i : ℤ), 0 ≤ i ∧ i < k ∧ p = (start.1 + i * dir.1, start.2 + i * dir.2) }
 
 -- Defines what a pairing strategy is in 2D.
 structure Pairing_strategy_2D (k : ℕ) where
