e2b88d1228bbcd20c806c026cbb8f22a6bc54753
Adam Zich <adam.zich3@gmail.com>
2026-03-23 23:56:22 +0100

    added stitching of all c++ files and running them to produce mnk.exe

---
+++ src/build.bat
@@ -0,0 +1,20 @@
+@echo off
+echo Cleaning old files...
+if exist mnk.exe del mnk.exe
+if exist *.o del *.o
+
+echo Compiling...
+g++ -std=c++17 main.cpp board.cpp util.cpp -o mnk.exe
+
+if %errorlevel% neq 0 (
+    echo.
+    echo [!] Compilation Failed.
+    pause
+    exit /b %errorlevel%
+)
+
+echo.
+echo [!] Build Successful! Running program...
+echo ---------------------------------------
+mnk.exe
+pause
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include "board.hpp"
 
 using namespace std;
 
 int main() {
+    Tiling idk(4, 5);
     return 0;
 }
