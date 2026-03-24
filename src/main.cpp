806252b11709e3b972115e1269ffbee58f9e737a
Adam Zich <adam.zich3@gmail.com>
2026-03-24 11:14:27 +0100

    fixed tiling constructor parameters not being met

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,14 @@
-#include <vector>
 #include <string>
 #include "board.hpp"
 
 using namespace std;
 
 int main() {
-    Tiling test1(4, 5);
+    Tiling test1(4, 5, 1);
     test1.PrintData();
-    Tiling test2(6, 3);
+    Tiling test2(6, 3, 1);
     test2.PrintData();
-    Tiling test3(4, 4);
+    Tiling test3(4, 4, 1);
     test3.PrintData();
     return 0;
 }
