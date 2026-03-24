5b39ff80976a2ca1545c0ed5bafa32224a2bcb9b
Adam Zich <adam.zich3@gmail.com>
2026-03-24 01:27:51 +0100

    updated required c++ version from 17 to 20 to use std::format()

--- src/build.bat
+++ src/build.bat
@@ -4,7 +4,7 @@ if exist mnk.exe del mnk.exe
 if exist *.o del *.o
 
 echo Compiling...
-g++ -std=c++17 main.cpp board.cpp util.cpp -o mnk.exe
+g++ -std=c++20 main.cpp board.cpp util.cpp -o mnk.exe
 
 if %errorlevel% neq 0 (
     echo.
