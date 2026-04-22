4b3c118060deb3003457a8a0571061fe32641d79
Adam Zich <adam.zich3@gmail.com>
2026-04-22 17:43:36 +0200

    more commenting and move val function tweaks

--- src/solvers/solvers.hpp
+++ src/solvers/solvers.hpp
@@ -10,18 +10,18 @@ public:
     int win_length = 0; // Length of line required to win (k in m,n,k)
     bool is_maker_maker = false; // Whether the solver is for a maker-maker game (as opposed to maker-breaker)
     bool is_two_player = true; // Whether the solver is for a two player game (as opposed to a one player puzzle)
-    double (*move_eval_function)(Tile& target, int playerId, Tiling* tiling) = nullptr; // Function pointer for the move evaluation function that evalueates the score of the given target tile
+    double (*move_eval_function)(Tile& target, int playerId, Tiling* tiling, int win_length, int depth, int maxDepth) = nullptr; // Function pointer for the move evaluation function that evalueates the score of the given target tile
     std::vector<Tile*> (*move_selection_function)(Tiling* tiling, int playerId) = nullptr; // Function pointer for the move selection function that selects the candidate moves to be evaluated at each step
     int max_depth = 0; // Maximum search depth for the solver (relevant for minimax based solvers)
 
     // Constructor
-    Solver(Tiling* tiling, int win_length, bool is_maker_maker, bool is_two_player, double (*move_eval_function)(Tile& target, int playerId, Tiling* tiling), std::vector<Tile*> (*move_selection_function)(Tiling* tiling, int playerId), int max_depth) {}
+    Solver(Tiling* tiling, int win_length, bool is_maker_maker, bool is_two_player, double (*move_eval_function)(Tile& target, int playerId, Tiling* tiling, int win_length, int depth, int maxDepth), std::vector<Tile*> (*move_selection_function)(Tiling* tiling, int playerId), int max_depth) {}
 
     // Destructor
     ~Solver() {}
 
 private:
-    Tile* selectBestMove(Tiling * tiling, int playerId) {}
+    Tile* selectBestMove(Tiling * tiling, int playerId, int win_length, int depth, int maxDepth) {}
 
     bool performPly(int playerId) {}
 
