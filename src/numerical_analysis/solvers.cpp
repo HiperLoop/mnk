e5fd3f99bd2e65cea98d7490381ee5df0a0992c0
Adam Zich <adam.zich3@gmail.com>
2026-05-08 17:18:10 +0200

    rearranged solver structure for more flexibility

--- src/numerical_analysis/solvers.cpp
+++ src/numerical_analysis/solvers.cpp
@@ -1,8 +1,8 @@
 #include "solvers.hpp"
 
 // Constructor
-Solver::Solver(Tiling* tiling, int win_length, bool is_maker_maker, bool is_two_player, double (*move_eval_function)(Tile& target, int playerId, Tiling* tiling, int win_length, int depth, int maxDepth), std::vector<Tile*> (*move_selection_function)(Tiling* tiling, int playerId), int max_depth) 
-    : tiling(tiling), win_length(win_length), is_maker_maker(is_maker_maker), is_two_player(is_two_player), move_eval_function(move_eval_function), move_selection_function(move_selection_function), max_depth(max_depth) {
+Solver::Solver(Tiling* tiling, int win_length, bool is_maker_maker, bool is_two_player, double (*state_eval_function)(Tile& target, int playerId, Tiling* tiling, int win_length, int depth, int maxDepth), std::vector<Tile*> (*move_selection_function)(Tiling* tiling, int playerId), int max_depth, Tile* (*selectBestMove)(int playerId, Tiling* tiling, int win_length, int maxDepth), double firstWinEval)
+    : tiling(tiling), win_length(win_length), is_maker_maker(is_maker_maker), is_two_player(is_two_player), state_eval_function(state_eval_function), move_selection_function(move_selection_function), max_depth(max_depth), selectBestMove(selectBestMove), firstWinEval(firstWinEval) {
         std::cout << std::format("Initialized solver with win length {}, maker-maker: {}, two-player: {}, max depth: {}.\n", win_length, is_maker_maker, is_two_player, max_depth);
     }
 
@@ -11,25 +11,8 @@ Solver::~Solver() {
     // Free any resources if necessary (currently none)
 }
 
-// Private method to select the best move for a given player using the move evaluation and selection functions
-Tile * Solver::selectBestMove(Tiling * tiling, int playerId, int win_length, int depth, int maxDepth) {
-    std::vector<Tile*> possible_moves = move_selection_function(tiling, playerId);
-    Tile* best_move = nullptr;
-    double best_score = -std::numeric_limits<double>::infinity();
-
-    for (Tile* move : possible_moves) {
-        double score = move_eval_function(*move, playerId, tiling, win_length, depth, maxDepth);
-        if (score > best_score) {
-            best_score = score;
-            best_move = move;
-        }
-    }
-
-    return best_move;
-}
-
 bool Solver::performPly(int playerId) {
-    Tile* best_move = selectBestMove(tiling, playerId, win_length, 0, max_depth);
+    Tile* best_move = selectBestMove(playerId, tiling, win_length, max_depth);
     if (best_move != nullptr) {
         GameLogic::ApplyMove(playerId, *best_move); // Apply the move for the given player ID
     }
@@ -43,12 +26,7 @@ bool Solver::performPly(int playerId) {
 }
 
 bool Solver::firstWins() {
-    for(int depth = 0; depth < max_depth; ++depth) {
-        for(int playerId = 1; playerId <= (is_two_player ? 2 : 1); ++playerId) {
-            if (performPly(playerId)) {
-                return playerId == 1; // Return true if the first player wins, false if the second player wins or it is a draw in a maker-maker game
-            }
-        }
-    }
-    return false; // If no winner after max_depth, return false
+    double startEval = state_eval_function(*(tiling->Tesselation[0]), 1, tiling, win_length, 0, max_depth);
+    std::cout << startEval << "\n";
+    return startEval == firstWinEval; // Return whether eval at start equals firstWinEval
 }
